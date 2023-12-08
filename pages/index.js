e61f0494bc58df05acea38c0fa6909449171aaae
Pedro Henrique <pedrohs21@outlook.com>
2023-12-08 18:10:30 +0000

    Ajuste do conteúdo da tela inicial para o conteúdo do AIPoint

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,28 @@
 function Home() {
   return (
-    <h1>
-      Sorria, seu sorriso ilumina o seu caminho e contagia o mundo ao seu redor!
-    </h1>
+    <div>
+      <h1>AIPoint</h1>
+      <p>
+        "AiPoint" é uma combinação de "AI" (abreviação de inteligência
+        artificial) e "Point" (ponto).
+      </p>
+      <p>
+        O nome sugere um ponto de referência ou um local centralizado para
+        discussões relacionadas à inteligência artificial. A escolha desse nome
+        reflete a ideia de que este seja um lugar onde entusiastas,
+        profissionais e interessados em inteligência artificial possam discutir
+        e compartilhar ideias, informações, notícias e discussões relevantes no
+        campo da inteligência artificial.
+      </p>
+      <p>
+        Este site está atualmente em desenvolvimento, mas convidamos você a
+        voltar futuramente para explorar e participar das discussões. Será um
+        espaço dinâmico e interativo, criado especialmente para a comunidade
+        interessada em inteligência artificial, proporcionando uma plataforma
+        aberta para expressar opiniões e perspectivas. Estamos ansiosos para
+        recebê-lo quando o site estiver pronto. Até logo!
+      </p>
+    </div>
   );
 }
 
