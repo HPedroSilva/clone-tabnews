f7f4d5c0288a7678c9f64acabef5beb81745f935
Pedro Henrique <pedrohs21@outlook.com>
2023-12-09 23:18:17 +0000

    Implementação do desafio de inseririnformações do banco de dados no endpoint `status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,23 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1+1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ a: "média" });
+  const updatedAt = new Date().toISOString();
+  const dbVersionResult = await database.query("show server_version;");
+  const dbMaxConnectionsResult = await database.query("show max_connections");
+  const dbConnectionsResult = await database.query(
+    "select sum(numbackends) from pg_stat_database;",
+  );
+
+  const dbVersion = dbVersionResult.rows[0].server_version;
+  const dbMaxConnections = dbMaxConnectionsResult.rows[0].max_connections;
+  const dbConnections = dbConnectionsResult.rows[0].sum;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    db_version: dbVersion,
+    db_max_connections: dbMaxConnections,
+    db_connections: dbConnections,
+  });
 }
 
 export default status;
